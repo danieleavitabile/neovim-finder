fada7ced108e53148a97f3b6f1e9792fd88d3d3f
Daniele Avitabile <d.avitabile@vu.nl>
2024-04-21 09:30:20 +0200

    Moved notes in Dropbox folder

--- src/neovimNote.tsx
+++ src/neovimNote.tsx
@@ -5,8 +5,10 @@ export default async () => {
   let script = `
 
         -- Name of the file to be opened
-        set filename to "~/Desktop/note.md"
-        set windowname to "note.md (~/Desktop) - NVIM"
+#        set filename to "~/Desktop/note.md"
+#        set windowname to "note.md (~/Desktop) - NVIM"
+        set filename to "~/Library/CloudStorage/Dropbox/Notes/note.md"
+        set windowname to "note.md (~/Library/CloudStorage/Dropbox/Notes) - NVIM"
 
         -- Set window bounds
         set x0 to 2500
@@ -24,16 +26,14 @@ export default async () => {
         end tell
 
         tell application "iTerm"
-          activate
           set hasNotes to exists window windowname 
           if isRunning and hasNotes then
             select first window where name is windowname
           else
-            if isRunning is false
-              set win1 to current window
-            else
-              set win1 to create window with default profile
-            end
+            if isRunning is false then
+              activate
+            end if
+            set win1 to create window with default profile
             set bounds of win1 to {x0, y0, x0 + dx, y0 + dy}
             tell current session of current window
                write text openCmd
